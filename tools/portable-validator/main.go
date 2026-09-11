ef3e12888e18de70c70f3d5aab17f47c29d921c0
alibinnadeem-git <alibinnadeem.official@gmail.com>
2026-09-11 11:07:51 -0700

    Add portable verify-plc command

--- tools/portable-validator/main.go
+++ tools/portable-validator/main.go
@@ -6,45 +6,27 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-		os.Exit(2)
-	}
+	if len(os.Args) < 2 { usage(); os.Exit(2) }
 	var err error
 	switch os.Args[1] {
-	case "init":
-		err = initCommand(os.Args[2:])
-	case "doctor":
-		err = doctorCommand(os.Args[2:])
-	case "verify-genesis":
-		err = verifyCanonicalGenesisCommand(os.Args[2:])
-	case "verify-genesis-trust":
-		err = verifyGenesisTrustCommand(os.Args[2:])
-	case "verify-snapshot":
-		err = verifySnapshotCommand(os.Args[2:])
-	case "verify-finality":
-		err = verifyFinalityCommand(os.Args[2:])
-	case "verify-validator-governance":
-		err = verifyValidatorGovernanceCommand(os.Args[2:])
-	case "verify-round-change":
-		err = verifyRoundChangeCommand(os.Args[2:])
-	case "verify-package":
-		err = verifyPackageCommand(os.Args[2:])
-	case "version":
-		fmt.Println(bootstrapVersion)
-		return
-	default:
-		usage()
-		os.Exit(2)
+	case "init": err = initCommand(os.Args[2:])
+	case "doctor": err = doctorCommand(os.Args[2:])
+	case "verify-genesis": err = verifyCanonicalGenesisCommand(os.Args[2:])
+	case "verify-genesis-trust": err = verifyGenesisTrustCommand(os.Args[2:])
+	case "verify-snapshot": err = verifySnapshotCommand(os.Args[2:])
+	case "verify-finality": err = verifyFinalityCommand(os.Args[2:])
+	case "verify-validator-governance": err = verifyValidatorGovernanceCommand(os.Args[2:])
+	case "verify-round-change": err = verifyRoundChangeCommand(os.Args[2:])
+	case "verify-plc": err = verifyPLCCommand(os.Args[2:])
+	case "verify-package": err = verifyPackageCommand(os.Args[2:])
+	case "version": fmt.Println(bootstrapVersion); return
+	default: usage(); os.Exit(2)
 	}
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR:", err)
-		os.Exit(1)
-	}
+	if err != nil { fmt.Fprintln(os.Stderr,"ERROR:",err); os.Exit(1) }
 }
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "STRATUM portable validator bootstrap")
-	fmt.Fprintln(os.Stderr, "commands: init, doctor, verify-genesis, verify-genesis-trust, verify-snapshot, verify-finality, verify-validator-governance, verify-round-change, verify-package, version")
-	fmt.Fprintln(os.Stderr, "This bootstrap creates CANDIDATE nodes only. It never grants vote authority.")
+	fmt.Fprintln(os.Stderr,"STRATUM portable validator bootstrap")
+	fmt.Fprintln(os.Stderr,"commands: init, doctor, verify-genesis, verify-genesis-trust, verify-snapshot, verify-finality, verify-validator-governance, verify-round-change, verify-plc, verify-package, version")
+	fmt.Fprintln(os.Stderr,"This bootstrap creates CANDIDATE nodes only. It never grants vote authority.")
 }
