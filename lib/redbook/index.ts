5d66f4eb77f3b00387c38880a146c59a886c900d
alibinnadeem-git <alibinnadeem.official@gmail.com>
2026-09-11 14:13:23 -0700

    Export authenticated peer transport schema

--- lib/redbook/index.ts
+++ lib/redbook/index.ts
@@ -7,6 +7,7 @@ export * from './schema/povi';
 export * from './schema/liveness';
 export * from './schema/plc';
 export * from './schema/vrf';
+export * from './schema/peer-transport';
 export * from './schema/validator-governance';
 export * from './validator-governance';
 export * from './finality-proof';
