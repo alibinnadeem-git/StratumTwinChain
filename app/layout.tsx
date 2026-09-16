43ab1694324a9c0f3b3b2a7285afd1f392c10ba5
alibinnadeem-git <alibinnadeem.official@gmail.com>
2026-09-16 09:19:54 -0700

    Load accessibility foundation globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import './responsive.css';
 import './redbook.css';
 import './task-ui.css';
+import './accessibility.css';
 import Shell from '@/components/Shell';
 import {STRATUM_PRODUCT} from '@/lib/redbook/terminology';
 
